bc0ad5fe94423564718c1323d657acef96f1f5cf
sonicpro <chegnenado@yahoo.com>
2024-03-13 15:40:06 +0200

    Added negative argument handling in Fibonacci.fs.

--- 1_BasicConcepts/Fibonacci/Fibonacci/Fibonacci.fs
+++ 1_BasicConcepts/Fibonacci/Fibonacci/Fibonacci.fs
@@ -1,11 +1,11 @@
 ﻿let rec fib = function
   | 0 -> 0
   | 1 -> 1
-  | n -> fib (n - 1) + fib (n - 2)
-  // TODO: add thowing when n < 0.
-  // | _ -> invalidArg "A parameter cannot be negative."
+  | n when n > 1 -> fib (n - 1) + fib (n - 2)
+  | _ -> failwith "Cannot evaluate fibonacci of a negative number."
 
 [<EntryPoint>]
 let main argv =
   printf "fib 10: %i" (fib 10)
+  printf "fib -10: %i" (fib -10)
   0
