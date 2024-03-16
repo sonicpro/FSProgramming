74ddf059da6ea280b0a3691f2f2aab56cb64fab6
sonicpro <chegnenado@yahoo.com>
2024-03-16 17:37:53 +0200

    Added anonymous functions demo.

---
+++ 5_HigherOrderFunctions/AnonymousFunctions/AnonymousFunctions/AnonymousFunctions.fs
@@ -0,0 +1,27 @@
+﻿open System.Diagnostics
+
+(* A demo that shows how to replace a parameterized function by parameterless function in place,
+   thanks to anonymous functions. *)
+
+// The f parameter is a parameterless function.
+let measure f =
+    let timer = new Stopwatch()
+    timer.Start()
+    let returnValue = f ()
+    printfn "Execution time is %i ms." timer.ElapsedMilliseconds
+    returnValue
+
+// fib is a function that has an int parameter.
+let fib n =
+    let rec loop previousFib accumulator counter =
+        if counter = n then
+            accumulator
+        else
+            loop accumulator (accumulator + previousFib) (counter + 1)
+    loop 1I 0I 0
+
+[<EntryPoint>]
+let main argv =
+    measure (fun () -> fib 5) |> ignore
+    measure (fun () -> fib 100) |> ignore
+    0
