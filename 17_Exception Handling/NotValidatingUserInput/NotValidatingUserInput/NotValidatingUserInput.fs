9ba6fb2cc7477fe3d4d2764cd65272a43a7663a8
sonicpro <chegnenado@yahoo.com>
2024-03-30 12:18:07 +0200

    Added Exception handling demos.

---
+++ 17_Exception Handling/NotValidatingUserInput/NotValidatingUserInput/NotValidatingUserInput.fs
@@ -0,0 +1,10 @@
+﻿let promptForANumber msg =
+    printf msg
+    int32(System.Console.ReadLine())
+
+[<EntryPoint>]
+let Main argv =
+    let x = promptForANumber("x = ")
+    let y = promptForANumber("y = ")
+    printf "%i + %i = %i" x y (x + y)
+    0
---
+++ 17_Exception Handling/ValidatingUserInput/ValidatingUserInput/Program.fs
@@ -0,0 +1,17 @@
+﻿let promptForANumber message =
+    printf message
+    try
+        int(System.Console.ReadLine())
+    with
+        // The patterns are of "Type Test Pattern" pattern type.
+        | :? System.FormatException -> System.Int32.MinValue
+        | :? System.OverflowException -> System.Int32.MinValue
+
+
+[<EntryPoint>]
+let main argv =
+    let x = promptForANumber "x = "
+    let y = promptForANumber "y = "
+
+    printfn "%i + %i = %i" x y (x + y)
+    0
